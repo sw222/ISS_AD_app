f0f9e384b24e15abbba7d2af6869e1a5ad197cfc
william <476855396@qq.com>
2015-09-07 15:47:26 +0800

    add http get method

--- app/src/main/java/com/tianhang/adapp/base/impl/ReceivePager.java
+++ app/src/main/java/com/tianhang/adapp/base/impl/ReceivePager.java
@@ -3,28 +3,110 @@ package com.tianhang.adapp.base.impl;
 import android.app.Activity;
 import android.graphics.Color;
 import android.view.Gravity;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.lidroid.xutils.ViewUtils;
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.AsyncHttpResponseHandler;
+import com.loopj.android.http.JsonHttpResponseHandler;
+import com.tianhang.adapp.R;
 import com.tianhang.adapp.base.BasePager;
+
+import org.apache.http.Header;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 /**
  * Created by student on 24/8/15.
  */
 public class ReceivePager extends BasePager{
-
+    private final String PATH = mActivity.getString(R.string.path);
+    private Button btn_get;
     public ReceivePager(Activity activity){
         super(activity);
     }
 
     @Override
-    public void initData() {
-        //tvTitle.setText("StatisticsPager");
+    public void initViews() {
+        super.initViews();
+        View view = View.inflate(mActivity, R.layout.pager_receive, null);
+        mRootView = view;
+        ViewUtils.inject(this, view); // 把当前的View对象注入到xUtils框架中
+        mRootView = view;
+        btn_get = (Button)mRootView.findViewById(R.id.id_get);
+        btn_get.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
-        TextView textView = new TextView(mActivity);
-        textView.setText("Receive");
-        textView.setTextColor(Color.RED);
-        textView.setTextSize(25);
-        textView.setGravity(Gravity.CENTER);
-        flContent.addView(textView);
+                //httpGet();
+                httpJsonGet();
+            }
+        });
+    }
+
+    @Override
+    public void initData() {
+        super.initData();
+
+    }
+
+    public void httpGet(){
+        String path = PATH+"/customer/haha";
+        AsyncHttpClient client = new AsyncHttpClient();
+        client.get(path, new AsyncHttpResponseHandler() {
+
+            @Override
+            public void onSuccess(int i, Header[] headers, byte[] bytes) {
+                String result = new String(bytes);
+                Toast.makeText(mActivity, "result" + result, Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onFailure(int i, Header[] headers, byte[] bytes, Throwable throwable) {
+
+                Toast.makeText(mActivity, "request network failed !", Toast.LENGTH_SHORT).show();
+
+            }
+        });
+    }
+    public void httpJsonGet() {
+        String path = PATH+"/customer/haha";
+        AsyncHttpClient client = new AsyncHttpClient();
+
+            client.get(path,null,new JsonHttpResponseHandler(){
+                @Override
+                public void onFailure(int statusCode, Header[] headers, String responseString, Throwable throwable) {
+                    super.onFailure(statusCode, headers, responseString, throwable);
+                    Toast.makeText(mActivity, "request network failed !", Toast.LENGTH_SHORT).show();
+                }
+
+                @Override
+                public void onSuccess(int statusCode, Header[] headers, JSONArray response) {
+                    super.onSuccess(statusCode, headers, response);
+                    //JSONObject firstEvent = response.get(0);
+                    //String tweetText = firstEvent.getString("text");
+
+                    // Do something with the response
+                    //System.out.println(tweetText);
+                }
+
+                @Override
+                public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+                    super.onSuccess(statusCode, headers, response);
+                    String address;
+                    try {
+                        address = response.get("Address").toString();
+                    }catch (JSONException e){
+                        //throw new JSONException("json exception !");
+                        throw new RuntimeException(e);
+                    }
+                    Toast.makeText(mActivity, "json-->"+address, Toast.LENGTH_SHORT).show();
+                }
+            });
+
     }
 }
